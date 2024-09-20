110ee07e24305aea1050ce724ef06aaaaeb9fee1
Vitor Alves Pereira <68354632+vitoralvesp@users.noreply.github.com>
2024-09-20 08:01:07 -0300

    comunicação entre as threads para andar e elevador funcionando

--- main.c
+++ main.c
@@ -1,110 +1,84 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
 
-
-// Elevator --> estrutura para conter os atributos do elevador
 typedef struct {
-  short floor;
-  short nextFloor;
+
+  short building_floors;
+
+  short elevator_floor;
+  short elevator_next_floor;
+
   short passengers;
-
-} Elevator;
-
-// Building --> estrutura para conter os atributos do prédio
-typedef struct {
-  short floors;
-  short elevators;
-  short floor_passengers;
-
-} Building;
-
-//
-typedef struct {
-  Building building;
-  Elevator elevator;
 
 } Args;
 
-
-// requisition(prédio) --> realiza a requisição do elevador em um andar gerado aleatoriamente
+// requisition(prédio) --> realiza a requisição do elevador em um andar gerado
+// aleatoriamente
 void *request(void *args);
 
 //
 void *move_elevator(void *args);
 
-
 int main() {
 
-  Building building = {6, 1, 0}; // Prédio de 6 andares e 1 elevador
-  Elevator elevator;
-  Args args = {building, elevator};
-  
-  pthread_t floor_thread;  
-  pthread_t elevator_thread;  
-  
-  
-  while(1) {
+  Args args = {6, 0, 0, 0};
 
-    elevator.floor = 0;
-    elevator.nextFloor = 0;
-    elevator.passengers = 0;
+  pthread_t floor_thread;
+  pthread_t args_thread;
 
-    printf(">> [INICIO] Elevador está no andar %d\n", elevator.floor);
+  while (1) {
 
     // realiza a requisição do elevador em um andar do prédio
-    pthread_create(&floor_thread, NULL, &request, (void *) &args);
+    pthread_create(&floor_thread, NULL, &request, (void *)&args);
     pthread_join(floor_thread, NULL);
 
     //
-    pthread_create(&elevator_thread, NULL, &move_elevator, (void *) &args);
-    pthread_join(elevator_thread, NULL);
-
-    printf(">> [FIM] Elevador está no andar %d\n", elevator.floor);
+    pthread_create(&args_thread, NULL, &move_elevator, (void *)&args);
+    pthread_join(args_thread, NULL);
     
   }
 
   pthread_exit(NULL);
   return 0;
-  
 }
 
-// request(prédio) --> realiza a requisição do elevador em um andar gerado aleatoriamente
+// request(prédio) --> realiza a requisição do elevador em um andar gerado
+// aleatoriamente
 void *request(void *args) {
 
-  Building *building = (Building *)args;
-  Elevator *elevator = (Elevator *)args;
+  Args *request_args = (Args *)args;
   
-  short random_floor = rand() % building->floors;
-  printf("---\n>> Requisição do elevador no andar %d\n", random_floor);
-  elevator->nextFloor = random_floor;
+  short random_floor = rand() % request_args->building_floors;
+  printf("-------------------------------------------------------\n\n");
+  printf(">> Requisição do elevador no andar %d\n", random_floor);
+  request_args->elevator_next_floor = random_floor;
 
   return NULL;
-  
 }
 
 //
 void *move_elevator(void *args) {
 
-  Building *building = (Building *)args;
-  Elevator *elevator = (Elevator *)args;
+  Args *move_args = (Args *)args;
 
-  while(!(elevator->floor == elevator->nextFloor)) {
+  while(!(move_args->elevator_floor == move_args->elevator_next_floor)) {
 
-    if (elevator->floor < elevator->nextFloor) {
-      printf(">> Elevador subindo...\n\t- Andar Atual: %d\n\t- Andar de Parada: %d\n\n", elevator->floor, elevator->nextFloor);
-      elevator->floor++;
-      
+    if (move_args->elevator_floor < move_args->elevator_next_floor) {
+      printf("\t- Elevador subindo...\n\t\t- Andar Atual: %hi\n\t\t- Andar de Parada: %hi\n", move_args->elevator_floor, move_args->elevator_next_floor);
+      move_args->elevator_floor++;
+
     } else {
-      printf(">> Elevador descendo...\n\t- Andar Atual: %d\n\t- Andar de Parada: %d\n\n", elevator->floor, elevator->nextFloor);
-      elevator->floor--;
-      
+      printf("\t- Elevador descendo...\n\t\t- Andar Atual: %hi\n\t\t- Andar de Parada: %hi\n", move_args->elevator_floor, move_args->elevator_next_floor);
+      move_args->elevator_floor--;
+
     }
     sleep(1); // 1 segundo de espera por andar
-    
+
   }
-  printf(">> Elevador chegou ao andar de parada: andar %d\n\n", elevator->floor);
+  printf("\t- Elevador chegou ao andar de parada: andar %d\n\n",
+  move_args->elevator_floor);
   
   return NULL;
 }
